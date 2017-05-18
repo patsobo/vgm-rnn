01229f6d784b24a39066f65e4abdde4f02cb326f
Patrick Sobolewski <psobolew@mymail.mines.edu>
2017-05-17 18:39:23 -0600

    Extracted midi file names.  Next step is to only grab specific ones and concatentate the entire download url.

--- link_parser.py
+++ link_parser.py
@@ -6,13 +6,23 @@ html_page = sys.argv[1]
 # create a subclass and override the handler methods
 class MyHTMLParser(HTMLParser):
 	def handle_starttag(self, tag, attrs):
-		print "Encountered a start tag:", tag
+		#print "Encountered a start tag:", tag
+		for attr in attrs:
+			# make sure you're reading a link
+			if attr[0] == 'href':
+				# check if it's a midi file
+				if (attr[1][-4:] == '.mid'):
+					# TODO: store in a data structure or file
+					print attr[1]
+		
 
 	def handle_endtag(self, tag):
-		print "Encountered an end tag :", tag
+		pass
+		#print "Encountered an end tag :", tag
 	
 	def handle_data(self, data):
-		print "Encountered some data  :", data
+		pass
+		#print "Encountered some data  :", data
 
 # instantiate the parser and fed it some HTML
 parser = MyHTMLParser()
